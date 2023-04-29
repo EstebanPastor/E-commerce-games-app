b79006350e717157596a98f70e8862ccd2b1e7ae
estebanpastor <estebanandrespastor@gmail.com>
2023-04-29 20:41:23 -0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import "./App.css"
 import Footer from "./components/footer/Footer";
+
 function App() {
   return (
     <>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,48 @@
-import "./footer.css"
+import "./footer.css";
 
 function Footer() {
-  return <div className="main-footer">
-   
-  </div>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="footer-col">
+            <h4>Steamcito</h4>
+            <ul>
+              <li>About us</li>
+              <li>Our services</li>
+              <li>Privacy policy</li>
+              <li>Affiliate program</li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Contact</h4>
+            <ul>
+              <li>FAQ</li>
+              <li>Games</li>
+              <li>Server status</li> 
+              <li>Order status</li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Created by:</h4>
+            <ul>
+              <li></li>
+              <li>Esteban</li>
+              <li>Galo</li>
+              <li>Mora</li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Follow us</h4>
+            <div className="social-links">
+              <a href="">All rights reserved</a>
+              <p className="copy">&copy;{new Date().getFullYear()}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
