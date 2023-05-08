6ae06744e1803396cb536dfe291ca84a17a12856
LucioGallucci <lucio.galogallucci@gmail.com>
2023-05-08 18:51:02 -0300

    añadir navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css"
 import Footer from "./components/footer/Footer";
-
+import Header from "./components/header/Header";
 function App() {
   return (
     <>
+      <div classname="App">
+        <Header
+          titulo='Steamcito'
+        />
+      </div>
       <Footer />
     </>
   );
