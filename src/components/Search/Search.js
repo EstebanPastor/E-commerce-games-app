56c726cea4f7700af202c1eac26053f48763f06a
Esteban Pastor <estebanandrespastor@gmail.com>
2023-05-24 21:22:11 -0300

    fixed some shit

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,32 +1,35 @@
-export const Search = () => {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-black">
-        <button
-          className="navbar-toggler"
-          type="button"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search a game"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-primary my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
-    </>
-  );
-};
+const Search = () => {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-black">
+          <button
+            className="navbar-toggler"
+            type="button"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search a game"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-primary my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </nav>
+      </>
+    );
+  };
+  
+  export default Search;
+  
